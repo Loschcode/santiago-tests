e10889b5873e227bd95046ec747ee102681b89ed
Laurent Schaffner <bonjour@laurentschaffner.com>
2016-04-05 04:52:26 +0200

    Update deck.rb

--- ruby/app/lib/card_game/deck.rb
+++ ruby/app/lib/card_game/deck.rb
@@ -3,7 +3,7 @@ class Deck
   attr_accessor :cards
 
   def initialize(number_cards)
-    @cards = (1..number_cards).to_a.shuffle.collect { |n| Card.new(n) }
+    @cards = (1..number_cards).to_a.shuffle.map { |n| Card.new(n) }
   end
 
 end
